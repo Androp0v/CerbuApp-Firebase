df5538da6f97726bc67bb84a991da51d270f7843
Raúl Montón Pinillos <raul_m_p@me.com>
2020-09-17 23:35:33 +0200

    Delete old check-ins while classifying.

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,9 @@ admin.initializeApp();
 exports.recountCapacities = functions.database.ref('/Capacities/Check-ins/{checkinid}').onWrite(async (change) => {
 	
 	// Grab the current (.after) value of what was written to the Realtime Database.
-    const checkinsRef = change.after.ref.parent;
+	const checkinsRef = change.after.ref.parent;
 	const countsRef = admin.database().ref('Capacities/Count/');
+	const capacitiesRef = admin.database().ref('Capacities/');
 
 	// Define a bunch of variables
 	var comedorCount = 0;
@@ -20,25 +21,44 @@ exports.recountCapacities = functions.database.ref('/Capacities/Check-ins/{check
 	const salaDeLecturaTimeout = 6*60*60; //6 horas
 	const bibliotecaTimeout = 6*60*60; //6 horas
 
-	// Retrieve all check-ins and classify them
+	const timeNow = Date.now();
+
+	// Save all node updates inside a single dictionary
+	const updates = {};
+
+	// Retrieve all check-ins and classify them, delete the old ones
 	const checkins = await checkinsRef.once('value');
+
 	checkins.forEach((child) => {
-      if (child.child("Room").val() === "Comedor") {
-        comedorCount += 1;
-      }else if (child.child("Room").val() === "SalaDeLectura"){
-      	salaDeLecturaCount += 1;
-      }else if (child.child("Room").val() === "Biblioteca"){
-      	bibliotecaCount += 1;
-      }
-    });
+		if (child.key != "Placeholder"){
+			if (child.child("Room").val() === "Comedor") {
+				if (timeNow - child.child("Time").val() >= comedorTimeout){
+					updates["Check-ins/" + child.key] = null;
+				}else{
+					comedorCount += 1;
+				}
+			} else if (child.child("Room").val() === "SalaDeLectura"){
+				if (timeNow - child.child("Time").val() >= salaDeLecturaTimeout){
+					updates["Check-ins/" + child.key] = null;
+				}else{
+					salaDeLecturaCount += 1;
+				}
+			} else if (child.child("Room").val() === "Biblioteca"){
+				if (timeNow - child.child("Time").val() >= bibliotecaTimeout){
+					updates["Check-ins/" + child.key] = null;
+				}else{
+					bibliotecaCount += 1;
+				}
+			}
+		}
+	});
 
-    // Update child nodes "Count" with freshly counted data
-    const counts = await countsRef.once('value');
-    const updates = {};
+	// Update child nodes "Count" with freshly counted data
+	const counts = await countsRef.once('value');
+	
+	updates["Count/Comedor/Current"] = comedorCount;
+	updates["Count/SalaDeLectura/Current"] = salaDeLecturaCount;
+	updates["Count/Biblioteca/Current"] = bibliotecaCount;
 
-    updates["Comedor/Current"] = comedorCount;
-    updates["SalaDeLectura/Current"] = salaDeLecturaCount;
-    updates["Biblioteca/Current"] = bibliotecaCount;
-
-  	return countsRef.update(updates);
+	return capacitiesRef.update(updates);
 });
